6f37513c4157c8fc47b0c709074192e9650dd819
nathanrice88 <33645139+nathanrice88@users.noreply.github.com>
2019-02-11 09:57:27 -0800

    Update initialize.sql
    Added users to user table and edited user table so that primary key is username, not an ID.

--- db/initialize.sql
+++ db/initialize.sql
@@ -1,39 +1,41 @@
 CREATE TABLE user (
-	user_id 	INT SERIAL PRIMARY KEY,
-	name 		  VARCHAR(255) NOT NULL
+	user_name 	VARCHAR(255) NOT NULL,
+	firstName	VARCHAR(255) NOT NULL,
+	lastName	VARCHAR(255) NOT NULL,
+	email		VARCHAR(255) NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY (user_name),
+	CONSTRAINT user_uni UNIQUE (email)
 );
 
 CREATE TABLE post (
-	poster_id	INT,
-	time	    TIME NOT NULL,
+	poster_name	VARCHAR(255) NOT NULL,
+	time	    	DATETIME2 NOT NULL,
 	title 		VARCHAR(255) NOT NULL,
-	message 	VARCHAR(255),
-  lati 		  FLOAT,
-  long  	  FLOAT,
-	CONSTRAINT post_pk PRIMARY KEY (poster_id, time),
-	CONSTRAINT post_fk_user FOREIGN KEY (poster_id)
-	REFERENCES user(user_id)
+	message 	VARCHAR(255) NOT NULL,
+  	lati 		FLOAT,
+  	long  	  	FLOAT,
+	CONSTRAINT post_pk PRIMARY KEY (poster_name, time),
+	CONSTRAINT post_fk_user FOREIGN KEY (poster_name)
+	REFERENCES user(user_name)
   );
   
 CREATE TABLE comment (
-	user_id	      INT,
-	poster_id	    INT,
-	post_time	    TIME,
-	comment_time	TIME NOT NULL,
-	text		      VARCHAR(255) NOT NULL,
-	CONSTRAINT comment_pk PRIMARY KEY (user_id, poster_id, post_time, comment_time),
-	CONSTRAINT comment_fk_user FOREIGN KEY (user_id)
-	REFERENCES user(user_id),
-	CONSTRAINT comment_fk_post PRIMARY KEY (poster_id, post_time)
-	REFERENCES post(poster_id, post_time)
+	user_name	VARCHAR(255) NOT NULL,
+	poster_name	VARCHAR(255) NOT NULL,
+	post_time	DATETIME2,
+	comment_time	DATETIME2 NOT NULL,
+	text		VARCHAR(255) NOT NULL,
+	CONSTRAINT comment_pk PRIMARY KEY (user_name, poster_id, post_time, comment_time),
+	CONSTRAINT comment_fk_user FOREIGN KEY (user_name)
+	REFERENCES user(user_name),
+	CONSTRAINT comment_fk_post FOREIGN KEY (poster_name, post_time)
+	REFERENCES post(poster_name, post_time)
   );
 
-INSERT INTO "user" (name)
-  VALUES ('User1'),
-  ('Al'),
-  ('Bob'),
-  ('Carl'),
-  ('Dale'),
-  ('Earl'),
-  ('Fred'),
-  ('Gary');
+INSERT INTO "user" (user_name, firstName, LastName, email)
+  VALUES
+  ('Test1','Nathan','Rice', 'n8@cambium5.com'),
+  ('Test2','Eric','Do','ericdo62497@gmail.com'),
+  ('Test3','Kyle','Mazza','kylemazza@gmail.com'),
+  ('Test4','Kian','Badie','kianbadie@gmail.com'),
+  ('Test5','Christian','Wance','christian.wance@gmail.com');
