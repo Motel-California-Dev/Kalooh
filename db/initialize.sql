41aa44634451dcf351f2bf2ff5b3813bf5c32aeb
nathanrice88 <33645139+nathanrice88@users.noreply.github.com>
2019-02-13 10:01:13 -0800

    Update initialize.sql

--- db/initialize.sql
+++ db/initialize.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE users (
 	user_name 	VARCHAR(255) NOT NULL,
 	firstName	VARCHAR(255) NOT NULL,
 	lastName	VARCHAR(255) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE post (
   	long  	  	FLOAT,
 	CONSTRAINT post_pk PRIMARY KEY (poster_name, time),
 	CONSTRAINT post_fk_user FOREIGN KEY (poster_name)
-	REFERENCES user(user_name)
+	REFERENCES users(user_name)
   );
   
 CREATE TABLE comment (
@@ -27,12 +27,12 @@ CREATE TABLE comment (
 	text		VARCHAR(255) NOT NULL,
 	CONSTRAINT comment_pk PRIMARY KEY (user_name, poster_id, post_time, comment_time),
 	CONSTRAINT comment_fk_user FOREIGN KEY (user_name)
-	REFERENCES user(user_name),
+	REFERENCES users(user_name),
 	CONSTRAINT comment_fk_post FOREIGN KEY (poster_name, post_time)
 	REFERENCES post(poster_name, post_time)
   );
 
-INSERT INTO "user" (user_name, firstName, LastName, email)
+INSERT INTO "users" (user_name, firstName, LastName, email)
   VALUES
   ('Test1','Nathan','Rice', 'n8@cambium5.com'),
   ('Test2','Eric','Do','ericdo62497@gmail.com'),
