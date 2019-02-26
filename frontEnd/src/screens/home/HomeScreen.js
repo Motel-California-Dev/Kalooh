64c29001025705c67fcd1179a2d6db414f90f933
Kian Badie <kianbadie@gmail.com>
2019-02-26 15:13:09 -0800

    Integrate Expo MapView to create barebones front end that displays map and current location.

--- frontEnd/src/screens/home/HomeScreen.js
+++ frontEnd/src/screens/home/HomeScreen.js
@@ -1,12 +1,64 @@
 import React from "react";
-import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { Constants, MapView, Location, Permissions } from 'expo';
 
 export default class LoginScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      mapRegion: null,
+      hasLocationPermissions: false,
+      locationResult: null
+    };
+  }
+
+  componentDidMount(){
+    this._getLocationAsync();
+  }
+
+  _handleMapRegionChange = (mapRegion) => {
+    console.log(mapRegion);
+    this.setState( { mapRegion } );
+  };
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if(status !== 'granted'){
+      this.setState( { locationResult: 'Permission to access location was denied'} );
+    } else {
+      this.setState( {hasLocationPermissions: 'true'} );
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState( { locationResult: JSON.stringify(location) } );
+
+    this.setState( { mapRegion: {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    } } );
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text>Home Screen</Text>
-      </SafeAreaView>
+      <View style={styles.container}>
+        {
+          this.state.locationResult === null ? 
+            <Text>Finding your current location...</Text> :
+            this.state.hasLocationPermissions === false ? 
+              <Text>Location permissions are not granted.</Text> : 
+                this.state.mapRegion === null ? 
+                  <Text>Map region doesn't exist.</Text> : 
+                  <MapView
+                    style={styles.map}
+                    region={this.state.mapRegion}
+                    onRegionChange={this.handleMapRegionChange}
+                    showsUserLocation={true}
+                  />
+        }
+      </View>
     );
   }
 }
@@ -17,5 +69,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#ffffff"
+  },
+  map: {
+    flex: 1,
+    alignSelf: 'stretch'
   }
 });
