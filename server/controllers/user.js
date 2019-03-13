63fb4c445d308aa24ac2093374584471e8bca2a9
kylemazza <kyleamazza@gmail.com>
2019-03-12 17:21:01 -0700

    Renamed userController to controller/user for lexical simplicity

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -61,18 +61,3 @@ exports.update = (req, res) => {
     });
 };
 
-
-
-// callback
-db.query('SELECT NOW() as now', (err, res) => {
-  if (err) {
-    console.log(err.stack)
-  } else {
-    console.log(res.rows[0])
-  }
-})
-
-// promise
-db.query('SELECT NOW() as now')
-  .then(res => console.log(res.rows[0]))
-  .catch(e => console.error(e.stack))
