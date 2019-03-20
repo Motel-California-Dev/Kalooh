dee049b6e748a3231150caf8634e2f92a39a1320
Xian <christian.wance@gmail.com>
2019-03-20 09:27:53 -0700

    a

--- db/initialize.sql
+++ db/initialize.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
 	email		VARCHAR(255) NOT NULL,
 	password	VARCHAR(255) NOT NULL,
 	CONSTRAINT user_pk PRIMARY KEY (ID),
-	CONSTRAINT user_email UNIQUE (email)	
+	CONSTRAINT user_email UNIQUE (email),	
 	CONSTRAINT user_UN UNIQUE (user_name)
 );
 
