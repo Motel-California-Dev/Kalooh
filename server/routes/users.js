704dbd56aad934efc7233112f0cbee6d62376f6d
kylemazza <kyleamazza@gmail.com>
2019-03-22 21:39:30 -0700

    Added Passport middleware to sensitive routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,14 @@ const router = require('express-promise-router')();
 const passport = require('../auth/passport');
 const UserController = require('../controllers/users.js');
 
-router.use(passport.authenticate('jwt', { session: false }));
-
 router.route('/')
-  .get(UserController.list)
+  .get(passport.authenticate('jwt', { session: false }), UserController.list)
   .post(UserController.create)
-  .patch(UserController.update)
-  .delete(UserController.delete);
+  .delete(passport.authenticate('jwt', { session: false }), UserController.delete);
 
 router.route('/:id')
-  .get(UserController.find);
+  .get(passport.authenticate('jwt', { session: false }), UserController.find)
+  .patch(passport.authenticate('jwt', { session: false }), UserController.update);
 
 module.exports = router;
 
