d8c7974a9db0323fe4f7ea3e1df5a9888eccd9a2
kylemazza <kyleamazza@gmail.com>
2019-03-22 21:38:35 -0700

    Removed session-related things

--- server/server.js
+++ server/server.js
@@ -9,20 +9,10 @@ const passport = require('./auth/passport');
 const app = express();
 
 app.disable('x-powered-by');
-app.use(session({
-  secret: process.env.SESSION_SECRET || "development_secret",
-  resave: false,
-  saveUninitialized: true,
-  cookie: { 
-    httpOnly: true,
-    secure: true 
-  }
-}));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(passport.session());
 
 routes(app);
 
