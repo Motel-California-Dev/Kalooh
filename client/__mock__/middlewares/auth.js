9738e0580bcd21650192e0fa538b11444a742dca
kyleamazza <kyleamazza@gmail.com>
2019-04-08 12:17:23 -0700

    Added login functionality to mock server

---
+++ client/__mock__/middlewares/auth.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+module.exports = (req, res, next) => {
+  const { method, path, body: { username, password }} = req;
+  if (method === 'POST' && path === '/login') {
+    const users = JSON.parse(fs.readFileSync('__mock__/db.json')).users;
+    console.log(users);
+
+    for(let i = 0; i < users.length; i++) {
+      if (username === users[i].username && password === users[i].password) {
+        return res.status(200).send(users[i]);
+      }
+    }
+
+    return res.status(404).send({ message: "Login failed." });
+  }
+};
