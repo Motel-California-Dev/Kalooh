e73372334d67f11ca9f7e120a79672b4d7e78648
Kian Badie <kianbadie@gmail.com>
2019-04-22 20:56:59 -0700

    comment out code related to PostCard because it is not included in the github

--- client/src/screens/search/SearchScreen.js
+++ client/src/screens/search/SearchScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, View, SafeAreaView, Text } from "react-native";
 import { SearchBar } from "react-native-elements";
 import axios from "../../../config/axios";
-import PostCard from "./PostCard";
+//import PostCard from "./PostCard";
 
 export default class SearchScreen extends React.Component {
   constructor(props) {
@@ -33,7 +33,7 @@ export default class SearchScreen extends React.Component {
     let postList = posts.map(postInfo => {
       return (
         <View key={postInfo.id}>
-          <PostCard post={postInfo} />
+          { /* <PostCard post={postInfo} /> */}
         </View>
       );
     });
